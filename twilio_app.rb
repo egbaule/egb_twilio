bc4449106bd50483aec0764f75df751fba24e875
egbaule <egbaule@iqe.com>
2014-06-08 14:41:47 -0400

    Addded \get_shipments

--- twilio_app.rb
+++ twilio_app.rb
@@ -25,4 +25,42 @@ class TwilioApp < Sinatra::Base
     # acknowledge the successful delivery of the message
     { request_id: request_id, summary: "SMS message sent" }.to_json + "\n"
   end
+  
+   # This is needed to get shipments & tracking numbers for the store (CCODE)
+   post '/get_shipments' do
+
+    begin
+      # authenticate_shipstation
+
+      # Shipstation doesn't record time information - just date, so round the parameter down
+      since = Time.parse(@config[:since]).utc.beginning_of_day.iso8601
+
+      @client.Shipments.filter("ModifyDate ge datetime'#{since}' and ShipDate ne null")
+      panda_result = @client.execute
+
+      # TODO - get shipping carrier, etc.
+      panda_result.each do |resource|
+        add_object :shipment, {
+          id: resource.ShipmentID.to_s,
+          tracking: resource.TrackingNumber,
+          order_id: resource.OrderID.to_s
+        }
+      end
+      @kount = panda_result.count
+
+      # return current timestamp so parameter updates on hub side
+      # NOTE: shipstation doesn't provide detail beyond date so we need to round it down in order
+      # to not miss any shipments
+      add_parameter 'since', Time.now.utc.beginning_of_day
+    rescue => e
+      # tell Honeybadger
+      # log_exception(e)
+
+      # tell the hub about the unsuccessful get attempt
+      result 500, "Unable to get shipments. Error: #{e.message}"
+    end
+
+    result 200, "Retrieved #{@kount} shipments"
+  end
+
 end
